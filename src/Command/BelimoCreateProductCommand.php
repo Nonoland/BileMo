7a236c05e544d7869c142c3d8fc2a3c8c16ed452
NolanA88 <nolan@agence-88.fr>
2023-10-23 21:24:13 +0200

    Add Command to create product

---
+++ src/Command/BelimoCreateProductCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'belimo:create-product',
+    description: 'Add product',
+)]
+class BelimoCreateProductCommand extends Command
+{
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+
+        $this->entityManager = $entityManager;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $product = new Product();
+
+        $product->setName($io->ask('Name ?'));
+        $product->setBrand($io->ask('Brand ?'));
+        $product->setGtin($io->ask('Gtin ?'));
+        $product->setImage($io->ask('Url image ?'));
+        $product->setSupplierPrice($io->ask('Supplier price ?'));
+        $product->setSuggestedPrice($io->ask('Suggested price ?'));
+        $product->setShortDescription($io->ask('Short description ?'));
+        $product->setDescription($io->ask('Description ?'));
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        return Command::SUCCESS;
+    }
+}
