8ce96f74862b4d35070842b0cd05aab5f7656b6a
NolanA88 <nolan@agence-88.fr>
2023-10-24 12:43:43 +0200

    Refactor Customer.php, CustomerRepository.php, ProductController.php, and routes to use the plural form of "products" instead of "product" in order to maintain consistency throughout the codebase.

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -17,23 +17,23 @@ class ProductController extends AbstractController
         $this->productRepository = $productRepository;
     }
 
-    #[Route('/products', name: 'app_product_list', methods: ['GET'])]
+    #[Route('/products', name: 'app_products_list', methods: ['GET'])]
     public function productList(): JsonResponse
     {
         return $this->json($this->getProductPageSchema());
     }
 
-    #[Route('/products/page/{page}', name: 'app_product_list_page')]
+    #[Route('/products/page/{page}', name: 'app_products_list_page')]
     public function productListPage(int $page): JsonResponse
     {
         return $this->json($this->getProductPageSchema($page));
     }
 
-    #[Route('/products/detail/{id}', name: 'app_product_detail', methods: ['GET'])]
+    #[Route('/products/detail/{id}', name: 'app_products_detail', methods: ['GET'])]
     public function productDetail(Product $product): JsonResponse
     {
         $data['data'] = $product->getData();
-        $data['links']['self'] = $this->generateUrl('app_product_detail', ['id' => $product->getId()]);
+        $data['links']['self'] = $this->generateUrl('app_products_detail', ['id' => $product->getId()]);
 
         return $this->json($data);
     }
@@ -47,16 +47,16 @@ class ProductController extends AbstractController
             $data['data'][] = [
                 'name' => $product->getName(),
                 'gtin' => $product->getGtin(),
-                'link' => $this->generateUrl('app_product_detail', ['id' => $product->getId()])
+                'link' => $this->generateUrl('app_products_detail', ['id' => $product->getId()])
             ];
         }
 
         if ($page != 1) {
-            $data['links']['prev'] = $this->generateUrl('app_product_list_page', ['page' => $page - 1]);
+            $data['links']['prev'] = $this->generateUrl('app_products_list_page', ['page' => $page - 1]);
         }
 
-        $data['links']['self'] = $this->generateUrl('app_product_list_page', ['page' => $page]);
-        $data['links']['next'] = $this->generateUrl('app_product_list_page', ['page' => $page + 1]);
+        $data['links']['self'] = $this->generateUrl('app_products_list_page', ['page' => $page]);
+        $data['links']['next'] = $this->generateUrl('app_products_list_page', ['page' => $page + 1]);
 
         return $data;
     }
