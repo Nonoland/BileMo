94eb75e124c96b581655121fa15d2469c479f1f6
NolanA88 <nolan@agence-88.fr>
2023-10-26 13:05:32 +0200

    Refactor DashboardController.php to redirect the dashboard to the ProductCrudController and update the title and favicon.

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setThousandsSeparator('')
+            ->setDecimalSeparator(',')
+            ->setSearchFields(['name', 'gtin'])
+            ;
+    }
+}
