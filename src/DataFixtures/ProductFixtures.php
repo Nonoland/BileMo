f95c25789966ab406329d9e39ed152c6efa67bd9
NolanA88 <nolan@agence-88.fr>
2023-10-26 23:12:17 +0200

    Add fixtures

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ProductFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        //Generate products
+        for($i = 1; $i <= 10000; $i++) {
+            $product = new Product();
+            $product->setName("Product $i");
+            $product->setDescription($faker->realText(500));
+            $product->setShortDescription($faker->realText(150));
+            $product->setBrand($faker->domainWord());
+            $product->setImage($faker->url());
+            $product->setGtin($faker->ean13());
+            $product->setSupplierPrice($faker->numberBetween(50, 9999));
+            $product->setSuggestedPrice($product->getSupplierPrice() + 50);
+            $product->setFeatures($faker->words($faker->numberBetween(2, 6)));
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/StoreFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Store;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class StoreFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        //Generate stores
+        for($i = 1; $i < 10; $i++) {
+            $store = new Store();
+            $store->setName($faker->domainWord());
+            $manager->persist($store);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProductFixtures::class
+        ];
+    }
+}
