98e3a1bdbe6627c0bf8df8625a3a022018f9c144
NolanA88 <nolan@agence-88.fr>
2023-10-26 16:56:42 +0200

    Refactor CustomerCrudController.php to use new fields and remove unnecessary code.

--- src/Controller/Admin/CustomerCrudController.php
+++ src/Controller/Admin/CustomerCrudController.php
@@ -8,6 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CustomerCrudController extends AbstractCrudController
 {
@@ -18,38 +21,19 @@ class CustomerCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        $store = AssociationField::new('store')
-            ->setLabel('Store')
-            ->setFormTypeOptions([
-                'class' => Store::class,
-                'choice_label' => 'name'
-            ])
-        ;
-
-        if (Crud::PAGE_NEW === $pageName || Crud::PAGE_EDIT === $pageName) {
-            return [
-                'lastname',
-                'firstname',
-                'email',
-                $store
-            ];
-        }
-
-        if (Crud::PAGE_DETAIL === $pageName) {
-            return [
-                'id',
-                'lastname',
-                'firstname',
-                'email',
-                $store
-            ];
-        }
-
-        return [
-            'lastname',
-            'firstname',
-            'email',
-            $store
+        $fields = [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('lastname'),
+            TextField::new('firstname'),
+            EmailField::new('email'),
+            AssociationField::new('store')
+                ->setLabel('Store')
+                ->setFormTypeOptions([
+                    'class' => Store::class,
+                    'choice_label' => 'name'
+                ])
         ];
+
+        return $fields;
     }
 }
