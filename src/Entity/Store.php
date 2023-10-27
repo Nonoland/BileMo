9edc153fe6fd04f8e5f1deff6ba63419258064b1
NolanA88 <nolan@agence-88.fr>
2023-10-27 13:58:43 +0200

    Fix syntax error in Store.php line 31.

--- src/Entity/Store.php
+++ src/Entity/Store.php
@@ -28,8 +28,7 @@ class Store
 
     public function __construct(
         private TagAwareCacheInterface $cache
-    )
-    {
+    ){
         $this->customers = new ArrayCollection();
         $this->users = new ArrayCollection();
     }
