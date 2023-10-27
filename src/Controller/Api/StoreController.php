af08f09d0d4f3edaa6fed96c1c8d90a156365643
NolanA88 <nolan@agence-88.fr>
2023-10-27 13:45:36 +0200

    Refactor StoreController.php to use caching for store details and store lists.

--- src/Controller/Api/StoreController.php
+++ src/Controller/Api/StoreController.php
@@ -7,15 +7,19 @@ use App\Repository\StoreRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[Route('/api', name: 'api_')]
 class StoreController extends RouteController
 {
 
     private StoreRepository $storeRepository;
-    public function __construct(StoreRepository $storeRepository)
+    private TagAwareCacheInterface $cache;
+    public function __construct(StoreRepository $storeRepository, TagAwareCacheInterface $cache)
     {
         $this->storeRepository = $storeRepository;
+        $this->cache = $cache;
     }
 
     #[Route(
@@ -52,30 +56,36 @@ class StoreController extends RouteController
         $this->verifyAccess($store);
 
         return $this->json($this->getObjectDetail(
-            $store->getData(),
+            $this->cache->get('storeDetail_' . $store->getId(), function (ItemInterface $item) use ($store) {
+                $item->tag('storesDetails');
+                return $store->getData();
+            }),
             $this->generateUrl('api_app_stores_detail', ['idStore' => $store->getId()])
         ));
     }
 
     private function getStorePageSchema(int $page = 1): array
     {
-        $stores = $this->storeRepository->findByPage($this->getUser(), $page);
+        return $this->cache->get("getStorePageSchema_" . $page . "_" . $this->getUser()->getId(), function (ItemInterface $item) use ($page) {
+            $item->tag('storeLists');
+            $stores = $this->storeRepository->findByPage($this->getUser(), $page);
 
-        $data['data'] = [];
-        foreach ($stores as $store) {
-            $data['data'][] = [
-                'name' => $store->getName(),
-                'link' => $this->generateUrl('api_app_stores_detail', ['idStore' => $store->getId()])
-            ];
-        }
+            $data['data'] = [];
+            foreach ($stores as $store) {
+                $data['data'][] = [
+                    'name' => $store->getName(),
+                    'link' => $this->generateUrl('api_app_stores_detail', ['idStore' => $store->getId()])
+                ];
+            }
 
-        if ($page != 1) {
-            $data['links']['prev'] = $this->generateUrl('api_app_stores_list_page', ['page' => $page - 1]);
-        }
+            if ($page != 1) {
+                $data['links']['prev'] = $this->generateUrl('api_app_stores_list_page', ['page' => $page - 1]);
+            }
 
-        $data['links']['self'] = $this->generateUrl('api_app_stores_list_page', ['page' => $page]);
-        $data['links']['next'] = $this->generateUrl('api_app_stores_list_page', ['page' => $page + 1]);
+            $data['links']['self'] = $this->generateUrl('api_app_stores_list_page', ['page' => $page]);
+            $data['links']['next'] = $this->generateUrl('api_app_stores_list_page', ['page' => $page + 1]);
 
-        return $data;
+            return $data;
+        });
     }
 }
