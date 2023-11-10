1b4bc4de2e82b44f365e8c6b43702b8caa32c884
NolanA88 <nolan@agence-88.fr>
2023-11-10 10:30:40 +0100

    Add new properties and their definitions to the ProductController.php file, including properties such as id, name, gtin, link, brand, image, supplier_price, suggested_price, short_description, description, and features. Also add new links property with self link.

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -33,6 +33,58 @@ class ProductController extends RouteController
     #[OA\Response(
         response: 200,
         description: 'Get products list',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(
+                        properties: [
+                            new OA\Property(
+                                property: 'id',
+                                type: 'integer'
+                            ),
+                            new OA\Property(
+                                property: 'name',
+                                type: 'string'
+                            ),
+                            new OA\Property(
+                                property: 'gtin',
+                                description: 'GTIN Code',
+                                type: 'string',
+                                pattern: '^\d{8}(\d{5})?$'
+                            ),
+                            new OA\Property(
+                                property: 'link',
+                                type: 'string',
+                                format: 'uri'
+                            ),
+                        ],
+                        maxItems: 10
+                    )
+                ),
+                new OA\Property(
+                    property: 'links',
+                    properties: [
+                        new OA\Property(
+                            property: 'self',
+                            type: 'string',
+                            format: 'uri'
+                        ),
+                        new OA\Property(
+                            property: 'next',
+                            type: 'string',
+                            format: 'uri'
+                        ),
+                        new OA\Property(
+                            property: 'prev',
+                            type: 'string',
+                            format: 'uri'
+                        ),
+                    ]
+                )
+            ]
+        )
     )]
     public function productList(): JsonResponse
     {
@@ -49,6 +101,58 @@ class ProductController extends RouteController
     #[OA\Response(
         response: 200,
         description: 'Get products list with page selector',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(
+                        properties: [
+                            new OA\Property(
+                                property: 'id',
+                                type: 'integer'
+                            ),
+                            new OA\Property(
+                                property: 'name',
+                                type: 'string'
+                            ),
+                            new OA\Property(
+                                property: 'gtin',
+                                description: 'GTIN Code',
+                                type: 'string',
+                                pattern: '^\d{8}(\d{5})?$'
+                            ),
+                            new OA\Property(
+                                property: 'link',
+                                type: 'string',
+                                format: 'uri'
+                            ),
+                        ],
+                        maxItems: 10
+                    )
+                ),
+                new OA\Property(
+                    property: 'links',
+                    properties: [
+                        new OA\Property(
+                            property: 'self',
+                            type: 'string',
+                            format: 'uri'
+                        ),
+                        new OA\Property(
+                            property: 'next',
+                            type: 'string',
+                            format: 'uri'
+                        ),
+                        new OA\Property(
+                            property: 'prev',
+                            type: 'string',
+                            format: 'uri'
+                        ),
+                    ]
+                )
+            ]
+        )
     )]
     public function productListPage(int $page): JsonResponse
     {
@@ -65,6 +169,75 @@ class ProductController extends RouteController
     #[OA\Response(
         response: 200,
         description: 'Get product detail',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    property: 'data',
+                    type: 'array',
+                    items: new OA\Items(
+                        properties: [
+                            new OA\Property(
+                                property: 'id',
+                                type: 'integer'
+                            ),
+                            new OA\Property(
+                                property: 'name',
+                                type: 'string'
+                            ),
+                            new OA\Property(
+                                property: 'gtin',
+                                description: 'GTIN Code',
+                                type: 'string',
+                                pattern: '^\d{8}(\d{5})?$'
+                            ),
+                            new OA\Property(
+                                property: 'brand',
+                                type: 'string'
+                            )
+                            ,
+                            new OA\Property(
+                                property: 'image',
+                                type: 'string',
+                                format: 'uri'
+                            ),
+                            new OA\Property(
+                                property: 'supplier_price',
+                                type: 'float'
+                            ),
+                            new OA\Property(
+                                property: 'suggested_price',
+                                type: 'float'
+                            ),
+                            new OA\Property(
+                                property: 'short_description',
+                                type: 'string'
+                            ),
+                            new OA\Property(
+                                property: 'description',
+                                type: 'string'
+                            ),
+                            new OA\Property(
+                                property: 'features',
+                                type: 'array',
+                                items: new OA\Items(
+                                    type: 'string'
+                                )
+                            )
+                        ]
+                    )
+                ),
+                new OA\Property(
+                    property: 'links',
+                    properties: [
+                        new OA\Property(
+                            property: 'self',
+                            type: 'string',
+                            format: 'uri'
+                        )
+                    ]
+                )
+            ]
+        )
     )]
     public function productDetail(
         #[MapEntity(mapping: ['idProduct' => 'id'])]
@@ -89,6 +262,7 @@ class ProductController extends RouteController
             $data['data'] = [];
             foreach ($products as $product) {
                 $data['data'][] = [
+                    'id' => $product->getId(),
                     'name' => $product->getName(),
                     'gtin' => $product->getGtin(),
                     'link' => $this->generateUrl('api_app_products_detail', ['idProduct' => $product->getId()])
